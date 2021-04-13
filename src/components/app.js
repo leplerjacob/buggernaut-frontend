cb333656e0a22908aead055c75794de18c301e97
Jacob Lepler <lepler.jacob@gmail.com>
2021-04-13 00:36:18 -0400

    before refactor to functional component

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { login } from "../actions/auth";
-import {connect, useDispatch} from 'react-redux'
+import { login, logout, checkIfLoggedIn } from "../actions/auth";
+import { connect, useDispatch } from "react-redux";
 
 class App extends Component {
   constructor(props) {
@@ -13,10 +13,22 @@ class App extends Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.fetchLogin = this.fetchLogin.bind(this);
-    
+    this.loginUser = this.loginUser.bind(this);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.checkUserIsLoggedIn = this.checkUserIsLoggedIn.bind(this)
   }
 
+  componentDidMount() {
+    let res = this.checkUserIsLoggedIn()
+
+    setTimeout(() => {
+
+    }, 10000)
+  }
+
+  checkUserIsLoggedIn = () => {
+    return this.props.checkIfLoggedIn()
+  }
 
   handleChange(e) {
     this.setState({
@@ -27,12 +39,16 @@ class App extends Component {
   handleSubmit(e) {
     e.preventDefault();
 
-    this.fetchLogin()
+    this.loginUser();
   }
 
-  fetchLogin = async () => {
-    const {username, password} = this.state;
-    await this.props.login(username, password, password)
+  loginUser = async () => {
+    const { username, password } = this.state;
+    await this.props.login(username, password, password);
+  };
+
+  logoutUser = async () => {
+    await this.props.logout();
   };
 
   render() {
@@ -64,7 +80,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  user: state.auth.user
-})
+  user: state.auth.user,
+});
 
-export default connect(mapStateToProps,{login})(App)
+export default connect(mapStateToProps, { login, logout, checkIfLoggedIn })(App);
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,15 +1,28 @@
 const initialState = {
-  user: {},
+  loggedInStatus: false,
+  user: "",
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case "LOGIN":
-      return {
-        ...state,
-        user: action.user,
-      };
+      const { user, logged_in, status } = action.user;
+      if (status !== 401) {
+        return {
+          ...state,
+          loggedInStatus: logged_in,
+          user: user,
+        };
+      }
+      if (status === 401) {
+        return {
+          ...state,
+          loggedInStatus: logged_in,
+          user: "",
+        };
+      }
+
     default:
       return state;
   }
-}
+};
